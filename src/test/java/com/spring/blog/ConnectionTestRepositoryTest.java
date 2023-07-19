708e5643e19a40501e2ddf5a9c4e8b1f1aeee724
songyoh <code6207@gmail.com>
2023-07-19 20:16:07 +0900

    블로그 프로젝트 마이바티스 환경설정 완료

---
+++ src/test/java/com/spring/blog/ConnectionTestRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.spring.blog;
+
+import com.spring.blog.repository.ConnectionTestRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // @Mapper로 적재된 마이바티스 인터페이스 인식을 위해 어노테이션 작성
+public class ConnectionTestRepositoryTest {
+
+    @Autowired // 테스트코드는 필드주입을 해도 괜찮은 경우가 많음
+    ConnectionTestRepository connectionTestRepository;
+
+    @Test
+    public void getNowTest(){
+        System.out.println("얻어온 현재 시간: "+ connectionTestRepository.getNow());
+    }
+}
